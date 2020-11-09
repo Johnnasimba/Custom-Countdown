035521d7e4dd86d96889a74df18d19d655562213
JOHN NASIMBA <nasimba4john@gmail.com>
2020-11-09 23:30:06 +0200

    Selected all elements that require update and save them to contant variables

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const countdownForm = document.getElementById('countdownForm');
 const inputContainer = document.getElementById('input-container');
 const dateEl = document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
+
 let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
