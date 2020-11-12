5cfb5d3cd24a4e178602e0f298f3ff71f33aa97c
JOHN NASIMBA <nasimba4john@gmail.com>
2020-11-12 22:44:51 +0200

    Selected on required elements incomplete section and save them to a constant variable

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const countdownElTitle = document.getElementById('countdown-title');
 const countdownBtn = document.getElementById('countdown-button');
 const timeElements = document.querySelectorAll('span');
 
+const completeEl = document.getElementById('complete');
+const completeElInfo = document.getElementById('complete-info');
+const completeBtn = document.getElementById('complete-button');
 
 let countdownTitle = '';
 let countdownDate = '';
